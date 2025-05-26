8733f4049a16932502852297bb034018ac710395
Maur025 <mmoyapalli@gmail.com>
2025-05-26 17:21:17 -0400

    feat: update socket.io adapter to use redis and implement bot management features

--- src/socket-topics.ts
+++ src/socket-topics.ts
@@ -2,6 +2,7 @@ export const SocketTopic = {
 	// base topics
 	CONNECTION: 'connection',
 	DISCONNECT: 'disconnect',
+	CONNECT: 'connect',
 	// room topics
 	ROOM_JOIN: 'room:join',
 	ROOM_LEAVE: 'room:leave',
@@ -9,4 +10,7 @@ export const SocketTopic = {
 	ROOM_LEAVE_RESPONSE: 'room:leave-response',
 	// device topics
 	DEVICE_LOCATION_LAST: 'device-location:last',
+	DEVICES_PUBLISHED_IN_KAFKA: 'devices:published-in-kafka',
+	// Server side events
+	PROPAGATE_ORDER_INITIALIZE_BOTS: 'propagate-order:initialize-bots',
 } as const;
---
+++ src/socket/client/socket-bridge-client.ts
@@ -0,0 +1,32 @@
+import env from '@config/env';
+import { loggerInfo } from '@maur025/core-logger';
+import { SocketTopic } from 'socket-topics';
+import { io, Socket } from 'socket.io-client';
+
+const SOCKET_NAME = 'socket-bridge-client-primary';
+
+const { CONNECT } = SocketTopic;
+
+let socketBridge: Socket | null = null;
+
+export const socketBridgeConnect = (): Socket => {
+	const socket: Socket = io(`http://localhost:${env.PORT}`, {
+		reconnection: true,
+	});
+
+	socket.on(CONNECT, () => {
+		loggerInfo(
+			`[${SOCKET_NAME}] connected to http://localhost:${env.PORT} with id '${socket.id}'`
+		);
+	});
+
+	return socket;
+};
+
+export const getSocketBridge = (): Socket => {
+	if (!socketBridge) {
+		socketBridge = socketBridgeConnect();
+	}
+
+	return socketBridge;
+};
